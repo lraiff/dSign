d853877c829a5ef4e0286ed12483b3025a5ef15c
Sally Shin <91296660+sassmander@users.noreply.github.com>
2022-04-28 12:56:01 -0400

    Finalized login page

--- login.php
+++ login.php
@@ -47,13 +47,21 @@ session_start();
 	}
 
 ?>
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>login</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+    .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+    .fa-anchor,.fa-coffee {font-size:200px}
+    </style>
 </head>
 <body>
 
@@ -99,9 +107,12 @@ session_start();
 		<form method="post">
 			<div style="font-size: 20px;color: white;">Login</div> <br>
 			<div style= 'font-size: 15px; color: white;'>Username: </div>
-			<input id="text" type="text" name="user_name"><br><br>
+			<input id="text" type="text" name="user_name" required="yes"><br><br>
 			<div style= 'font-size: 15px; color: white;'>Password: </div>
-			<input id="text" type="password" name="password"><br><br>
+			<input id="text" type="password" name="password" required="yes"
+			pattern = "(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10}" 
+			title="Passwords contain at least one number and one uppercase and lowercase letter, and are 10 characters long"
+			><br><br>
 
 			<input id="button" type="submit" value="Login"><br><br>
 
@@ -109,4 +120,4 @@ session_start();
 		</form>
 	</div>
 </body>
-</html>
+</html> 
