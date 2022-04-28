50a83fbfed46a9759e987454fb045d7fe060b314
Sally Shin <91296660+sassmander@users.noreply.github.com>
2022-04-28 14:06:28 -0400

    Finalized guest_access

--- guest_access.php
+++ guest_access.php
@@ -34,7 +34,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
 
 ?>
 
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
   <head>
     <title>Guest</title>
@@ -48,25 +48,63 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
     body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
     .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
     .fa-anchor,.fa-coffee {font-size:200px}
+     body {
+		background-image: url('https://us.123rf.com/450wm/arinashe/arinashe1904/arinashe190400323/123496911-genome-sequencing-pattern-in-bright-colors-background-in-doodle-style-dna-genome-scissors-test-tube-.jpg?ver=6');
+    }
+
+ .card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: auto;
+  border-radius: 8px;
+  color: #FFF;
+  background-color: #FFF !important;
+  margin-left:5%;
+  margin-right:5%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 10px 10px;
+  color: #000;
+}
     </style>
   </head>
 
+  <body>
+
   <!-- Navbar -->
 <div class="w3-top">
-  <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+<div class="w3-bar w3-red w3-card w3-left-align w3-large">
     <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    
     <a href="http://localhost/dSign/index.php" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
-    <a href="https://www.dsign.w3spaces.com/index.html" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">About</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">FAQ</a>
-  </div>
 
-  <!-- Navbar on small screens -->
-  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-    <a href="https://www.dsign.w3spaces.com/index.html" class="w3-bar-item w3-button w3-padding-large">About</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Profile</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Sequence</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">FAQ</a>
-  </div>
+    <?php if( isset($_SESSION['loggedIn'])) { ?>
+    <a href="<?php echo "http://localhost/dSign/user_profile.php"; ?>" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Profile</a>
+    <?php } else { ?>
+    <a href="<?php echo "http://localhost/dSign/login.php"; ?>" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Profile</a>
+    <?php } ?>
+
+    <?php if( isset($_SESSION['loggedIn'])) { ?>
+    <a href="<?php echo "http://localhost/dSign/signatureInput.php"; ?>" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Signature</a>
+    <?php } else { ?>
+    <a href="<?php echo "http://localhost/dSign/login.php"; ?>" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Signature</a>
+    <?php } ?>
+
+    <a href="http://localhost/dSign/FAQ.html" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">FAQ</a>
+
+    <?php if( isset($_SESSION['loggedIn'])) { ?>
+    <a href="<?php echo "http://localhost/dSign/logout.php"; ?>" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Logout</a>
+    <?php } else { ?>
+    <a href="<?php echo "http://localhost/dSign/index.php"; ?>" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Logout</a>
+    <?php } ?>
+    
+    <a href="http://localhost/dSign/guestLogin.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Guest</a>
+</div>
 </div>
 
 <!-- Header -->
@@ -75,10 +113,10 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
   <hr>
   <p class="w3-large">View and export the encrypted sequence sent to you</p>
 </header>
-
-<div class="w3-row-padding w3-padding-64 w3-container">
-  <div class="w3-content">
-    <div class="w3-onethird">
+<br>
+<br>
+<div class="card">
+  <div class="container">
       <h3>Message</h3>
       <fieldset>
       <?php 
@@ -90,12 +128,12 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
         $finalMessage = rtrim($trimmed2, $charr2);
         echo print($finalMessage);?>
       <p class="w3-text-grey"></p>
-
+      <hr>
       <input type = "submit" value = "Export" id = "Export" name = "Export" class="w3-button w3-red w3-padding-large w3-small w3-margin-top">
       </fieldset>
     </div>
   </div>
-</div>
-
-
-
+<br>
+<br>
+</body>
+</html>
